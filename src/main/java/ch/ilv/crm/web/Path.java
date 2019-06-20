5c8c207ecf0784b594d1d1957af1158542976823
Titus Villiger <titus.villiger@gibmit.ch>
2019-06-20 10:23:41 +0200

    pom: utf8

---
+++ src/main/java/ch/ilv/crm/web/Path.java
@@ -0,0 +1,17 @@
+package ch.ilv.crm.web;
+
+public class Path {
+
+    public static class Web {
+        public static final String INDEX = "/index";
+        public static final String CUSTOMER = "/customer";
+        public static final String CREATE_CUSTOMER = "/createcustomer";
+    }
+
+    public static class Template {
+    	public static final String INDEX = "public/index.html";
+        public static final String NOT_FOUND = "public/notFound.html";
+        public static final String CUSTOMER = "public/customer.html";
+    }
+
+}
